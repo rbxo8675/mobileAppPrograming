c1947a2af1f05f6c99175a6b70177dc70bcb9ec3
rbxo8675 <rbxo8675@gmail.com>
2025-07-26 14:59:18 +0900

    bottomBar size

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -16,27 +16,28 @@ class  MyApp extends StatelessWidget {
 
     return MaterialApp(//실제로 코딩이 이루어지는 공간
         home: Scaffold(
-          appBar: AppBar(
+          appBar: AppBar(//상단 바
             backgroundColor: Colors.blue,
               title: Text("앱임",
               style: TextStyle(color: Colors.white),)
           ),
           body: Text("안녕"),
-          bottomNavigationBar: BottomAppBar(
-            child : Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-          Icon(Icons.phone),
-          Icon(Icons.message),
-          Icon(Icons.contact_page),
+          bottomNavigationBar: BottomAppBar(//하단 바
+            child : SizedBox(//row 위젯을 사용하기 위해서 컨테이너로 감싸줌
+              //컨테이너 위젯에 린트가 뜨는 이유 : 컨테이너는 무겁기 때문에 SizedBox를 사용
+              height: 100, //높이 설정
+              child: Row( //가로로 배치
 
-          ],
-        )
-
-
-
-    ),
-    ),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,//정렬
+                children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+                ],
+              ),
+            )
+          ),
+        ),
 
     );
 
