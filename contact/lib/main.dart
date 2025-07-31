38c99898026275f4ecf73c561de0564a42783640
rbxo8675 <rbxo8675@gmail.com>
2025-07-31 23:54:29 +0900

    플러터 숙제2

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -17,11 +17,22 @@ class  MyApp extends StatelessWidget {
     return MaterialApp(//실제로 코딩이 이루어지는 공간
         home: Scaffold(
           appBar: AppBar(//상단 바
-            title:
+            backgroundColor: Colors.white, //상단 바 배경색
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(1.0),
+              child: Container(
+                color: Colors.grey[300],
+                height: 1.0,
+              ),
+            ),
+              title:
             Row(
               children: [
                 Text("금호동 3가"),
-                Icon(Icons.keyboard_arrow_down),
+                IconButton(
+                    icon: Icon(Icons.keyboard_arrow_down),
+                        onPressed: (){},
+                ),
               ],
             ),
 
@@ -31,16 +42,17 @@ class  MyApp extends StatelessWidget {
                 onPressed: (){},
               ),
               IconButton(
-              icon:Icon(Icons.notifications), //검색 아이콘
-              onPressed: (){},
-              ),
-              IconButton(
                 icon:Icon(Icons.menu_sharp), //채팅 아이콘
                 onPressed: (){},
               ),
+              IconButton(
+              icon:Icon(Icons.notifications), //검색 아이콘
+              onPressed: (){},
+              ),
 
 
-            ]
+
+            ],
              //알림 아이콘
           ),
 
@@ -48,20 +60,47 @@ class  MyApp extends StatelessWidget {
           Container(
             child: Row(
               children: [
-                Image.asset(
-                  "assets/images/1.png", //이미지 경로
-                  width: 100, //너비
-                  height: 100, //높이
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.all(10.0), //컨테이너 밖에 여
+
+                  width: 100,
+                  height: 100,
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      "assets/images/canonCamera.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-                Column( //열
+          Column(
+            children: [
+              Text("캐논 DSLR 100D (단렌즈, 충전기 16R기가SD 포함)"),
+              Text("성동구 행당동  . 끌올 10분 전"),
+              Text("210,000원"),
+
+              Container(
+                child: Row(
                   children: [
-                    Text("캐논 DSLR 100D (단렌즈, 충전기 16R기가SD 포함)"),
-                    Text("성동구 행당동  . 끌올 10분 전"),
-                    Text("210,000원"),
-                    Icon(Icons.favorite_border), //하트 아이콘
+                    Icon(Icons.favorite_border, color: Colors.grey),
+                    Text("4", style: TextStyle(color: Colors.grey)),
                   ],
                 ),
-
+              ),
+            ],
+                ),
 
               ],
 
@@ -69,7 +108,7 @@ class  MyApp extends StatelessWidget {
           )
         )
 
-        );
+    );
 
 
 
