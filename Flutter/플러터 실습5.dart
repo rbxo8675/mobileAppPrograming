012fc3256aef8ee3d4707672e2ceee8a6e8a66f5
rbxo8675 <rbxo8675@gmail.com>
2025-08-07 23:25:19 +0900

    실습 진행

--- Flutter/플러터 실습5.dart
+++ Flutter/플러터 실습5.dart
@@ -1,3 +1,30 @@
 /*
-
+퍼센트로 크기를 지정할 수 있는 위젯은 Flexible 위젯입니다.
 */
+
+return MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(),
+    body: Row(
+      children: [
+        // 3: 7 , 5: 5는 절반
+        Flexible(child: Container(), flex : 3),
+        Flexible(child: Container()flex : 7),
+      ],
+    ),
+    ),
+);
+
+return MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(),
+    body: Row(
+      children: [
+        // 3: 7 , 5: 5는 절반
+        Expanded(child: Container()), // flex : 1 가진 flexible 위젯
+        Container(child: Container()),
+      ],
+    ),
+    ),
+);
+
